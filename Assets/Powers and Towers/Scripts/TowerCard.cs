6c5ef34ced4ca0410b8082a5026f926d8f9a7d02
Kevin <kevinspel@trebrorsor.se>
2021-12-15 14:06:16 +0100

    hm

--- Assets/Powers and Towers/Scripts/TowerCard.cs
+++ Assets/Powers and Towers/Scripts/TowerCard.cs
@@ -38,6 +38,27 @@ public class TowerCard : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         //    Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
+    private bool ViablePlacementArea
+    {
+        get
+        {
+            Collider2D objs;
+            objs = Physics2D.OverlapBox(Mouse.current.position.ReadValue(), Vector2.one, 0,6);
+            if (objs != null)
+            print(objs.name);
+            return objs == null;
+            //RaycastHit2D hit = Physics2D.BoxCast(Mouse.current.position.ReadValue(),
+            //    Vector2.one,
+            //    0,
+            //    Vector2.zero,
+            //    100,
+            //    6);
+            //if (hit)
+            //print(hit.collider.gameObject.name);
+            //return !hit;
+        }
+    } 
+
     public void OnPointerDown(PointerEventData eventData)
     {
         HoldingCard = true;
@@ -45,5 +66,15 @@ public class TowerCard : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerUp(PointerEventData eventData)
     {
         HoldingCard = false;
+        tweener.StartCoroutine("GoToStart");
+        if (ViablePlacementArea)
+        {
+            towerData.SetTower(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) + new Vector3(0, 0, 10));
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(Mouse.current.position.ReadValue(), Vector3.one);
     }
 }
--- Assets/Powers and Towers/Scripts/TowerData.cs
+++ Assets/Powers and Towers/Scripts/TowerData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 [CreateAssetMenu(fileName ="New Tower",menuName ="Towers/New Tower")]
@@ -8,7 +9,7 @@ public class TowerData : ScriptableObject
     public float attackSpeed = 1f;
     public int damage = 1;
     public DamageType type;
-
+    public Image image;
     public GameObject TowerPrefab;
 
     public void SetTower(Vector3 pos) 
