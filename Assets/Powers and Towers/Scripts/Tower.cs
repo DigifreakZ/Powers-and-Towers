791c57ed9b64613e59465451d6ac6088123d6bf6
Kevin <kevinspel@trebrorsor.se>
2021-12-15 10:24:03 +0100

    towers

---
+++ Assets/Powers and Towers/Scripts/Tower.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] float attackSpeed = 1f;
+    [SerializeField] int damage = 1f;
+}
