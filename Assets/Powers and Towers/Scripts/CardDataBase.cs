9c1fa73971f7307835614688389afeb3abeb7b6a
Kevin <kevinspel@trebrorsor.se>
2021-12-15 15:47:32 +0100

    f

--- Assets/Powers and Towers/Scripts/CardDataBase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName ="afaf",menuName ="aagdgsg")]
-public class CardDataBase :ScriptableObject
-{
-    public TowerData[] towerdatas;
-}
--- Assets/Powers and Towers/Scripts/MapManager.cs
+++ Assets/Powers and Towers/Scripts/MapManager.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public static MapManager instance;
-
+    public GameObject enemyPrefab;
     [SerializeField] private Transform[] _pathNodes;
 
     private void Awake()
     {
         if (MapManager.instance != null) Destroy(gameObject);
         MapManager.instance = this;
+        StartCoroutine("Spawner");
     }
     public static Transform[] GetPath()
     {
         if (MapManager.instance == null)
         {
-            Debug.LogWarning("No MapManager: MapManager.cs");
+            Debug.LogWarning("No Map Manager: MapManager.cs");
             return null;
         }
 
         return instance._pathNodes;
+    }
+    IEnumerator Spawner()
+    {
+        Instantiate(enemyPrefab, _pathNodes[0].position, Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(0.5f, 2f));
     }
 }
